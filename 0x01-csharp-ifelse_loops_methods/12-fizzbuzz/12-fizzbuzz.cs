afd22a0db5cb2adc7623834049756e89c974ebea
Ben Keener <keener4christ@gmail.com>
2020-07-14 17:19:33 -0400

    Completed task 12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            string text = "";
+
+            if (i % 3 == 0)
+                text += "Fizz";
+            if (i % 5 == 0)
+                text += "Buzz";
+
+            if (text == "")
+                text = i.ToString();
+
+            if (i == 0)
+                Console.Write(text);
+            else
+                Console.Write(" " + text);
+        }
+        Console.WriteLine("");
+    }
+}

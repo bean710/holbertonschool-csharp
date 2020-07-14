b3aa8d8a8ed1d18dd5e03df76f83ee75a3c83500
Ben Keener <keener4christ@gmail.com>
2020-07-14 12:46:36 -0400

    Completed task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb/6-print_comb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                if (i == 0 && j == 1)
+                {
+                    Console.Write("{0}{1}", i, j);
+                }
+                else
+                {
+                    Console.Write(", {0}{1}", i, j);
+                }
+            }
+        }
+        Console.WriteLine("");
+    }
+}

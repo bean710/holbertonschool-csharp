eabfce5b0bbea9852dc100170f563322cbdcaf7a
Ben Keener <keener4christ@gmail.com>
2020-07-14 12:41:20 -0400

    Completed task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i == 99)
+                Console.WriteLine(i);
+            else
+                Console.Write("{0}, ", i);
+        }
+    }
+}

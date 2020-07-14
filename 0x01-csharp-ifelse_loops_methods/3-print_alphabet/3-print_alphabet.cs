d754054fe2946c620a95683c3496d5c9d992cff4
Ben Keener <keener4christ@gmail.com>
2020-07-14 12:29:33 -0400

    Completed task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabet/3-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char i = 'a'; i <= 'z'; i++)
+        {
+            if (i != 'q' && i != 'e')
+                Console.Write(i);
+        }
+    }
+}

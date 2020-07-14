36a759b101d5eb9933b0219fd6a22dba26a02f0e
Ben Keener <keener4christ@gmail.com>
2020-07-14 16:34:10 -0400

    Changed to WriteLine

--- 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -11,6 +11,6 @@ class Program
         else if (number < 0)
             Console.WriteLine("{0} is negative", number);
         else
-            Console.Write("{0} is zero", number);
+            Console.WriteLine("{0} is zero", number);
     }
 }

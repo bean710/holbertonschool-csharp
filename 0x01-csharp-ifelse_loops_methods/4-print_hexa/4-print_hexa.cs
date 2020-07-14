9c6cc56249f1909c44909e5e24a07ddd8390fb15
Ben Keener <keener4christ@gmail.com>
2020-07-14 16:48:20 -0400

    Changed to lowecase hex

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         for (int i = 0; i <= 98; i++)
-            Console.WriteLine("{0} = 0x{0:X}", i);
+            Console.WriteLine("{0} = 0x{0:x}", i);
     }
 }

2a24dd4ea7554b6b19a99f34297526bc6c14055a
Ben Keener <keener4christ@gmail.com>
2020-07-16 10:12:19 -0400

    Completed task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] ret = new int[size];
+
+        for (int i = 0; i < ret.Length; i++)
+        {
+            ret[i] = i;
+
+            if (i == ret.Length - 1)
+            {
+                Console.Write(ret[i]);
+            }
+            else
+            {
+                Console.Write($"{ret[i]} ");
+            }
+        }
+        Console.WriteLine("");
+
+        return ret;
+    }
+}

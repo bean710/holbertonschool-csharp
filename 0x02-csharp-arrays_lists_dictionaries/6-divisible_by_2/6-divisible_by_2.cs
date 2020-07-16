5715a655dc97286cb888e51e1337bfaa1ca648c3
Ben Keener <keener4christ@gmail.com>
2020-07-16 11:11:16 -0400

    Completed task 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> ret = new List<bool>();
+
+        foreach (int val in myList)
+        {
+            ret.Add(val % 2 == 0);
+        }
+
+        return ret;
+    }
+}

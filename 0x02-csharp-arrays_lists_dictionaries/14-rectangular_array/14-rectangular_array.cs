864fbfb67c2eef99b2ec940b2eb24c6a2aeaa601
Ben Keener <keener4christ@gmail.com>
2020-07-16 13:04:16 -0400

    Completed task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] arr2d = new int[5, 5];
+
+        arr2d[2, 2] = 1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (j == 4)
+                {
+                    Console.WriteLine(arr2d[i, j]);
+                }
+                else
+                {
+                    Console.Write($"{arr2d[i, j]} ");
+                }
+            }
+        }
+    }
+}

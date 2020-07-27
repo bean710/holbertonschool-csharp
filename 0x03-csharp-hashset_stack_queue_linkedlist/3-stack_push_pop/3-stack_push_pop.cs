9bd4cbae37427dd3b342da4d9d8cce9ffc046feb
Ben Keener <keener4christ@gmail.com>
2020-07-27 16:36:12 -0400

    Completed task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+
+        Console.WriteLine("Top item: {0}", aStack.Peek());
+
+        bool foundSearch = aStack.Contains(search);
+        Console.WriteLine("Stack contains {0}: {1}", search, foundSearch);
+
+        if (foundSearch)
+        {
+            while (aStack.Peek() != search)
+            {
+                aStack.Pop();
+            }
+            aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}

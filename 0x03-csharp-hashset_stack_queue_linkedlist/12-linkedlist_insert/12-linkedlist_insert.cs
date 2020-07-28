6ed4d57805ada565977cb9b288f9c8c82daf3b76
Ben Keener <keener4christ@gmail.com>
2020-07-28 00:50:54 -0400

    Completed task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> node = myLList.First;
+        int index = 0;
+        while (node != null)
+        {
+            if (n == index)
+            {
+                return myLList.AddBefore(node, n);
+            }
+
+            node = node.Next;
+            index++;
+        }
+
+        return null;
+    }
+}

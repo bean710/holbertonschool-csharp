45a2763f585d9b4f401fe40dd38c9ddc3cbb0866
Ben Keener <keener4christ@gmail.com>
2020-07-28 00:37:09 -0400

    Completed task 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int value = myLList.First.Value;
+        myLList.RemoveFirst();
+        return value;
+    }
+}

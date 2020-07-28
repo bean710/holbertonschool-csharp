5ac92426fc7352952d96b607ae4dd4e3f70c65a9
Ben Keener <keener4christ@gmail.com>
2020-07-28 14:06:44 -0400

    Completed task 1

---
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+enum Rating
+{
+    Goood,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}

efa62b8af11a11e7e7b580b1535c5a3ce1797d2c
Ben Keener <keener4christ@gmail.com>
2020-07-28 01:08:40 -0400

    Added case for no nodes

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,6 +5,10 @@ class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
+        if (myLList.Count == 0)
+        {
+            return 0;
+        }
         int value = myLList.First.Value;
         myLList.RemoveFirst();
         return value;

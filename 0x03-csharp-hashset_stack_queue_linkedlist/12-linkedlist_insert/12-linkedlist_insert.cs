3f0850a6c6228d51d263be6d1f236644d4b05f13
Ben Keener <keener4christ@gmail.com>
2020-07-28 01:49:49 -0400

    Added least and greatest node edge cases

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -7,9 +7,14 @@ class LList
     {
         LinkedListNode<int> node = myLList.First;
 
+        if (n < node.Value)
+        {
+            return myLList.AddBefore(node, n);
+        }
+
         while (node != null)
         {
-            if (n >= node.Value && n < node.Next.Value)
+            if (n >= node.Value && (node.Next == null || n < node.Next.Value))
             {
                 return myLList.AddAfter(node, n);
             }

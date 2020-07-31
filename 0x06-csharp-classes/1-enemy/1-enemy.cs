93471d22d351b3536e7903608d4c6cf7784385fb
Ben Keener <keener4christ@gmail.com>
2020-07-31 00:42:16 -0400

    Completed task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// A simple Zombie class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// The health of the Zombie
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Constructor for the Zombie
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}

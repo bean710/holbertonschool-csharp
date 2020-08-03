732e6736ef1d3821d8f2263047ce1e6e487b6dd4
Ben Keener <keener4christ@gmail.com>
2020-08-03 14:47:35 -0400

    Completed task 4

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// Class containing methods for strings.
+    /// </summary>
+    public class Str
+    {
+        public static int UniqueChar(string s)
+        {
+            int[] counter = new int[26];
+
+            foreach (char c in s.ToCharArray())
+            {
+                counter[c - 'a'] += 1;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (counter[s[i] - 'a'] == 1)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}

0d58a5f21742da6c7da57505ad3903753d3cd3fe
Ben Keener <keener4christ@gmail.com>
2020-08-03 14:59:35 -0400

    Completed task 5

---
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(1, "foo")]
+        [TestCase(2, "fooBar")]
+        [TestCase(3, "fooBarBaz")]
+        [TestCase(0, "")]
+        [TestCase(0, ".!123")]
+        [TestCase(1, ".!123bar")]
+        public void CamelCase_NormalString_ReturnsNumberOfWords(int expected, string s)
+        {
+            int result = Text.Str.CamelCase(s);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}

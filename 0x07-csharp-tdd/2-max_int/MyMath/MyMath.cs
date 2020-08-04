b1e03c383f41c8fe0c915a1cde3dfad91c3a158b
Ben Keener <keener4christ@gmail.com>
2020-08-04 12:44:03 -0400

    Added null check

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -16,7 +16,7 @@ namespace MyMath
         /// <returns>The largest number in the list</returns>
         public static int Max(List<int> nums)
         {
-            if (nums.Count == 0)
+            if (nums == null || nums.Count == 0)
                 return 0;
 
             return nums.Max();

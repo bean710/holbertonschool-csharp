c22e86b991ed9874b9f61fe3aa9fe8618ccaa6d3
Ben Keener <keener4christ@gmail.com>
2020-08-06 21:56:35 -0400

    Completed task 2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Class for methods on objects
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if a class is a subclass of another class
+    /// </summary>
+    /// <param name="derivedType">The type to check if it is a subclass</param>
+    /// <param name="baseType">The type to check if derrivedType is a subclass of it</param>
+    /// <returns>True if subclass, otherwise false.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return (derivedType.IsSubclassOf(baseType));
+    }
+}

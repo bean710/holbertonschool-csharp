f2eff0352f1a381318424284161ff524f935d903
Ben Keener <keener4christ@gmail.com>
2020-08-06 21:00:10 -0400

    Completed task 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Class to contain methods for objects
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if an object is an int
+    /// </summary>
+    /// <param name="obj">The object to check</param>
+    /// <returns>True if the object is an int, otherwise false.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return (obj.GetType() == typeof(int));
+    }
+}

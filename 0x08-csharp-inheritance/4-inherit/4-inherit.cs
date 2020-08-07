cd4eeec5b9741c64cf30e2f32daeb2781a30dee2
Ben Keener <keener4christ@gmail.com>
2020-08-06 22:14:55 -0400

    Completed task 4

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Empty base Animal class
+/// </summary>
+class Animal
+{
+
+}
+
+/// <summary>
+/// Empty dog class, inheriting Animal
+/// </summary>
+class Dog : Animal
+{
+    
+}

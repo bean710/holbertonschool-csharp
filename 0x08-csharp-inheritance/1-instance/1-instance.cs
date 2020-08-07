36b0eff46f5be6f6c9decf07fe5d81958518f189
Ben Keener <keener4christ@gmail.com>
2020-08-06 21:13:53 -0400

    Completed task 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Class to conatin object methods
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Gets if an object is an instance of Array
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if instance, otherwise false</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return (obj is Array);
+    }
+}

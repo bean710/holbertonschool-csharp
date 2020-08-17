27c7f9fbc2c739bbbba9c25efab49f080f8658d1
Ben Keener <keener4christ@gmail.com>
2020-08-17 15:37:38 -0400

    Changed inverted to inverse

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -29,7 +29,7 @@ class ImageProcessor
                 }
             }
 
-            bm.Save($"{name}_inverted{extension}");
+            bm.Save($"{name}_inverse{extension}");
         }
     }
 }

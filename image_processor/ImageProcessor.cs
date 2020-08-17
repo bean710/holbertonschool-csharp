f920a058f2b46b7971520a850cf9dd3ab8b0c85c
Ben Keener <keener4christ@gmail.com>
2020-08-17 15:52:36 -0400

    Added threading

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
+using System.Threading;
 
 /// <summary>
 /// Class for image processing methods
@@ -15,21 +16,27 @@ class ImageProcessor
     {
         foreach (String filename in filenames)
         {
-            string name = Path.GetFileNameWithoutExtension(filename);
-            string extension = Path.GetExtension(filename);
-
-            Bitmap bm = new Bitmap(filename);
-            
-            for (int i = 0; i < bm.Height; i++)
-            {
-                for (int j = 0; j < bm.Width; j++)
-                {
-                    Color og = bm.GetPixel(j, i);
-                    bm.SetPixel(j, i, Color.FromArgb(255 - og.R, 255 - og.G, 255 - og.B));
-                }
-            }
-
-            bm.Save($"{name}_inverse{extension}");
+            Thread t = new Thread(()=>InvImg(filename));
+            t.Start();
         }
+    }
+
+    private static void InvImg(string filename)
+    {
+        string name = Path.GetFileNameWithoutExtension(filename);
+        string extension = Path.GetExtension(filename);
+
+        Bitmap bm = new Bitmap(filename);
+        
+        for (int i = 0; i < bm.Height; i++)
+        {
+            for (int j = 0; j < bm.Width; j++)
+            {
+                Color og = bm.GetPixel(j, i);
+                bm.SetPixel(j, i, Color.FromArgb(255 - og.R, 255 - og.G, 255 - og.B));
+            }
+        }
+
+        bm.Save($"{name}_inverse{extension}");
     }
 }

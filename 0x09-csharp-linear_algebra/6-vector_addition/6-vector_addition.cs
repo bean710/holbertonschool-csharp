31491827f2ce3a0ef3032d790e972fa9bb70fd5e
Ben Keener <keener4christ@gmail.com>
2020-08-25 22:52:35 -0400

    Completed task 6

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3)
+        {
+            double[] ret = new double[vector1.Length];
+
+            for (uint i = 0; i < vector1.Length; i++)
+            {
+                ret[i] = vector1[i] + vector2[i];
+            }
+
+            return ret;
+        }
+    }
+}

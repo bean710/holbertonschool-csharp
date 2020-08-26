85f384dcd70c14872bf83056aa9e3a35a9046002
Ben Keener <keener4christ@gmail.com>
2020-08-26 17:45:04 -0400

    Completed task 26

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        if (matrix.Rank != 2 || matrix.GetLength(0) != matrix.GetLength(1) ||
+            matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+            return -1;
+
+        if (matrix.GetLength(0) == 2)
+        {
+            return Math.Round(matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0], 2);
+        }
+        else
+        {
+            double sum = 0;
+            sum += matrix[2, 1] * matrix[1, 2] - matrix[1, 1] * matrix[2, 2];
+            sum -= matrix[1, 0] * matrix[2, 2] - matrix[2, 0] * matrix[1, 2];
+            sum += matrix[1, 0] * matrix[2, 1] - matrix[2, 0] * matrix[1, 1];
+            return sum;
+        }
+    }
+}

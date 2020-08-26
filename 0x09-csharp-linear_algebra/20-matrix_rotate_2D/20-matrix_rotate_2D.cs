3146a5aa7a96d9e9e6e7b625143a0c18a7e1ea92
Ben Keener <keener4christ@gmail.com>
2020-08-26 14:45:42 -0400

    Completed task 20

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        if (matrix.Rank != 2 || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[1,1] { {-1} };
+
+        double[,] ret = new double[2,2];
+
+        double cos = Math.Cos(angle);
+        double sin = Math.Sin(angle);
+
+        ret[0, 0] = Math.Round(cos * matrix[0, 0], 2);
+        ret[0, 1] = Math.Round(-sin * matrix[0, 1], 2);
+        ret[1, 0] = Math.Round(sin * matrix[1, 0], 2);
+        ret[1, 1] = Math.Round(cos * matrix[1, 1], 2);
+
+        return ret;
+    }
+}

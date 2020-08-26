0ec9f43c19ed8cc3d014d92e3cea19bc85e48714
Ben Keener <keener4christ@gmail.com>
2020-08-26 11:49:40 -0400

    Completed task 9

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+            return new double[1] {-1};
+
+        return (from num in vector select num * scalar);
+    }
+}

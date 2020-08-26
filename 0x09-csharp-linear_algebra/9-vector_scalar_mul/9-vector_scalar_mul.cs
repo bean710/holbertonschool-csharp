03c175c6ac1fe3ac5ada964c9d147619832185b3
Ben Keener <keener4christ@gmail.com>
2020-08-26 12:43:55 -0400

    Added documentation

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 using System.Linq;
 
+/// <summary>
+/// Contains Vector Math methods
+/// </summary>
 class VectorMath
 {
+    /// <summary>
+    /// Multiplies a vector by a scalar
+    /// </summary>
+    /// <param name="vector">A vector of doubles</param>
+    /// <param name="scalar">The scalar to multiply the vector by</param>
+    /// <returns>A new vector </returns>
     public static double[] Multiply(double[] vector, double scalar)
     {
         if (vector.Length < 2 || vector.Length > 3)

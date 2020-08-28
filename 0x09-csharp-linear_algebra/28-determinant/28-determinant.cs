ee039717e1c428bff2b029057420476edc75d87c
Ben Keener <keener4christ@gmail.com>
2020-08-28 16:35:43 -0400

    Fixed logic for 3D matrix

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -15,7 +15,7 @@ class MatrixMath
         else
         {
             double sum = 0;
-            sum += matrix[0, 0] * (matrix[2, 1] * matrix[1, 2] - matrix[1, 1] * matrix[2, 2]);
+            sum += matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[2, 1] * matrix[1, 2]);
             sum -= matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[2, 0] * matrix[1, 2]);
             sum += matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[2, 0] * matrix[1, 1]);
             return Math.Round(sum, 2);

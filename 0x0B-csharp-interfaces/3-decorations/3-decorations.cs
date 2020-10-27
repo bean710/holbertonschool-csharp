5e392d2d4062ac5e3048dc3bd0d6daa0bbf56afb
Ben Keener <keener4christ@gmail.com>
2020-10-27 15:18:13 -0400

    Added documentation

--- 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
+++ 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+/// <summary>
+/// A base class for all of the game objects
+/// </summary>
 abstract class Base
 {
     public string name { get; set; }
@@ -10,40 +13,77 @@ abstract class Base
     }
 }
 
+/// <summary>
+/// Defines the properties of an interactive object
+/// </summary>
 interface IInteractive
 {
     void Interact();
 }
 
+/// <summary>
+/// Defines the properties of a breakable object
+/// </summary>
 interface IBreakable
 {
     int durability { get; set; }
 }
 
+/// <summary>
+/// Defines the properties of a collectable object
+/// </summary>
 interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
 }
 
+/// <summary>
+/// A class for door mechanics
+/// </summary>
 class Door : Base, IInteractive
 {
+    /// <summary>
+    /// Constructor for a Door
+    /// </summary>
+    /// <param name="name">Optional: Name of the door, defaults to "Door"</param>
     public Door(string name = "Door")
     {
         this.name = name;
     }
 
+    /// <summary>
+    /// Interact with the door, will give the user output
+    /// </summary>
     public void Interact()
     {
         Console.WriteLine($"You try to open the {this.name}. It's locked.");
     }
 }
 
+/// <summary>
+/// A class for decoration mechanics
+/// </summary>
 class Decoration : Base, IInteractive, IBreakable
 {
+    /// <summary>
+    /// How much durability the decoration has
+    /// </summary>
+    /// <value>gets/sets the durability property of the decoration</value>
     public int durability { get; set; }
+
+    /// <summary>
+    /// Whether or not the decoration is a quest item
+    /// </summary>
+    /// <value>gets/sets the quest item property of the decoration</value>
     public bool isQuestItem { get; set; }
 
+    /// <summary>
+    /// Constructor for a decoration
+    /// </summary>
+    /// <param name="name">Optional: The name for the decoration to have. Defaults to "Decoration"</param>
+    /// <param name="durability">Optional: How much durability the decoration should have. Defaults to 1</param>
+    /// <param name="isQuestItem">Optional: Whether or not the decoration is a quest item. Defaults to false</param>
     public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
     {
         if (durability <= 0)
@@ -54,6 +94,9 @@ class Decoration : Base, IInteractive, IBreakable
         this.isQuestItem = isQuestItem;
     }
 
+    /// <summary>
+    /// Interact with the decoration. Provides the user with output
+    /// </summary>
     public void Interact()
     {
         if (durability <= 0)
@@ -71,6 +114,9 @@ class Decoration : Base, IInteractive, IBreakable
         Console.WriteLine($"You look at the {name}. Not much to see here.");
     }
     
+    /// <summary>
+    /// Reduce the durability of the decoration
+    /// </summary>
     public void Break()
     {
         durability--;
